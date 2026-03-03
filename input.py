f1c62fb4213a41054c0ac581904ccc8c2ba74074
j-shiji9 <shijijoel2002@gmail.com>
2026-03-03 17:32:36 +0000

    Testing input

---
+++ input.py
@@ -0,0 +1,10 @@
+# Input for 5 cities 
+x = input("Please select the weather for one of the following cities: city1, city2, city3, city4, city5: ")
+print(f"You have selected: {x}")
+
+
+
+
+# Import current date and time - 
+
+
